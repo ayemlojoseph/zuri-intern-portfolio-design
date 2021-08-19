f057f6d2fadf872f1ceeb7793dbd5107dee3cf96
root <ayemlojoseph@gmail.com>
2021-08-19 05:27:47 +0100

    Corrected settings

--- project/settings.py
+++ project/settings.py
@@ -122,7 +122,7 @@ STATIC_ROOT = (os.path.join(BASE_DIR,'static'),)
 
 
 django_heroku.settings(locals())
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
